477bf7fe297770e597091e7bc496603ae1073159
jason penno <pennojabc@gmail.com>
2018-05-28 16:54:45 +1000

    trying to add lighting

--- OpenGLProject/OpenGLProject/src/Headders/Shader.h
+++ OpenGLProject/OpenGLProject/src/Headders/Shader.h
@@ -31,8 +31,11 @@ public:
 	// Set uniforms
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 	void SetUniform3f(const std::string& name, glm::vec3 value);
+	void SetUniform1f(const std::string& name, float v0);
 	void SetUniform1i(const std::string& name, int value);
 	void SetuniformMat4f(const std::string& name, const glm::mat4& matrix);
+	void SetuniformMat3f(const std::string& name, const glm::mat3& matrix);
+
 private:
 	unsigned int CreateShader(const std::string &vertexShader, const std::string &fragmentShader);
 	unsigned int CompileShader(unsigned int type, const std::string &source);
